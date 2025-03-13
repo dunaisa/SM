2f1b23da3a71871805b265c76617dab2cc9b5e3e
admin <admin@Elizabeth.local>
2025-03-13 23:46:07 +0300

    add section winner & slider

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
 import './styles/style.scss';
 
-const message: string = 'Hello, TypeScript with SCSS!';
-console.log(message);
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.winners__cards-container', {
+    modules: [Navigation],
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 3.5,
+    slidesPerGroup: 1,
+    spaceBetween: 12,
+    navigation: {
+      nextEl: '.cards__btn_next',
+      prevEl: '.cards__btn_prev',
+    },
+  });
+
+  
+});
+
+  
