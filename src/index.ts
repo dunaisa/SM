876a7dbdf07b57043348c8cef4284c4ba26c232b
admin <admin@Elizabeth.local>
2025-03-14 19:54:11 +0300

    add links

--- src/index.ts
+++ src/index.ts
@@ -20,4 +20,36 @@ document.addEventListener('DOMContentLoaded', () => {
   
 });
 
+// const pixels = document.querySelectorAll('.pixel');
+
+// pixels.forEach((pixel) => {
+//     const randomDelay = Math.random() * 0.9; // Случайная задержка от 0 до 2 секунд
+//     pixel.style.animationDelay = `${randomDelay}s`;
+// });
+
+// // Функция для запуска анимации пикселей
+// function startPixelAnimation() {
+//   const pixels = document.querySelectorAll('.pixel');
+//   pixels.forEach((pixel) => {
+//       const randomDelay = Math.random() * 2; // Случайная задержка от 0 до 2 секунд
+//       pixel.style.animationDelay = `${randomDelay}s`;
+//       pixel.style.opacity = 1; // Запуск анимации
+//   });
+// }
+
+// // Создаем IntersectionObserver
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//           // Если блок в области видимости, запускаем анимацию
+//           startPixelAnimation();
+//           // Отключаем observer после первого срабатывания
+//           observer.unobserve(entry.target);
+//       }
+//   });
+// });
+
+// // Находим блок и начинаем наблюдать за ним
+// const pixelContainer = document.getElementById('pixel-container');
+// observer.observe(pixelContainer);
   
