a80df316c9d4b1c4114df9cfb889173e39650230
admin <admin@Elizabeth.local>
2025-03-16 12:58:28 +0300

    add blog section

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,20 @@ import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.winners__cards-container', {
+  const winnersSwiper = new Swiper('#winners-slider', {
+    modules: [Navigation],
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 3.5,
+    slidesPerGroup: 1,
+    spaceBetween: 12,
+    navigation: {
+      nextEl: '.cards__btn_next',
+      prevEl: '.cards__btn_prev',
+    },
+  });
+
+  const blogSwiper = new Swiper('#blog-slider', {
     modules: [Navigation],
     direction: 'horizontal',
     loop: false,
